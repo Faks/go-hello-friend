6fcce89bec10b75e8c9651c5c6eee65b8db9575c
Oskars Germovs <oskars_germovs@inbox.lv>
2024-09-18 23:34:25 +0300

    Hello, friend.

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, friend. Hello, friend? That’s lame. Maybe I should give you a name, but that’s a slippery slope. You’re only in my head. We have to remember that. Shit.!")
+}
